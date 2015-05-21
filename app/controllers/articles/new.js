b1c0be89046f43c9e7fb3c462b8ca758baad2632
Luis Edimerchk Laverde <edyhack@gmail.com>
2015-05-21 09:05:55 -0500

    lend articles working

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.description',
+    function () {
+      return !Ember.isEmpty(this.get('model.description'))
+    }
+  ),
+  actions: {
+    save: function () {
+      if (this.get('isValid')){
+        return true;
+      } else {
+        this.set('errorMessage', 'Error!! you have to fill the description!');
+      }
+    }
+  }
+});
