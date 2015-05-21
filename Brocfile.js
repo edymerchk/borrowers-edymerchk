e447d3305545eab5656d7c68dc7e9ea23be551b3
Luis Edimerchk Laverde <edyhack@gmail.com>
2015-05-21 15:05:56 -0500

    consume AMD library

--- Brocfile.js
+++ Brocfile.js
@@ -16,6 +16,13 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+app.import('bower_components/borrowers-dates/index.js', {
+  exports: {
+    'borrowers-dates': [
+      'format'
+    ]
+  }
+});
 
 app.import('bower_components/moment/moment.js');
 
