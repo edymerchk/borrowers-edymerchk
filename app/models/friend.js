60bd7aa70c4c8a4a78e8ee39640bbe9f9d7fc000
Luis Edimerchk Laverde <edyhack@gmail.com>
2015-05-22 12:03:19 -0500

    using ember-computed-change-gate

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import changeGate from 'ember-computed-change-gate/change-gate';
 
 export default DS.Model.extend({
   articles:      DS.hasMany('articles', {async: true}),
@@ -8,7 +9,22 @@ export default DS.Model.extend({
   email:         DS.attr('string'),
   twitter:       DS.attr('string'),
   totalArticles: DS.attr('number'),
-  fullName: Ember.computed('firstName', 'lastName', function() {
-    return this.get('firstName') + ' ' + this.get('lastName');
+  fullName: Ember.computed('firstName', 'lastName', function(key, value, oldValue) {
+    if (arguments.length === 1) {
+      // Setter
+      return this.get('firstName') + ' ' + this.get('lastName');
+    } else {
+      // Getter
+      var name = value.split(' ');
+
+      this.set('firstName', name[0]);
+      this.set('lastName', name[1]);
+
+      return value;
+    }
+
+  }),
+  capitalizedFirstName: changeGate('firstName', function(firstName) {
+    return Ember.String.capitalize(firstName);
   })
 });
