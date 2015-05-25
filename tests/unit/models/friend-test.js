182a48b1e84c12261e9d7b62a4af117f353cec51
Luis Edimerchk Laverde <edyhack@gmail.com>
2015-05-25 13:28:34 -0500

    writing some unit tests

--- tests/unit/models/friend-test.js
+++ tests/unit/models/friend-test.js
@@ -1,12 +1,32 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('friend', 'Unit | Model | friend', {
-  // Specify the other units that are required for this test.
-  needs: []
+moduleForModel('friend', 'Friend', {
+  needs: ['model:article']
 });
 
 test('it exists', function(assert) {
   var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
+  assert.ok(model);
+});
+
+
+test('fullName joins first and last name', function(assert) {
+  var model = this.subject({firstName: 'Luis', lastName: 'Laverde'});
+  assert.equal(model.get('fullName'), 'Luis Laverde');
+
+  Ember.run(function() {
+    model.set('firstName', 'Edymerchk');
+  });
+
+  assert.equal(model.get('fullName'), 'Edymerchk Laverde', 'Updates fullName');
+});
+
+
+test('articles relationship', function(assert) {
+  var klass = this.subject({}).constructor;
+
+  var relationship = Ember.get(klass, 'relationshipsByName').get('articles');
+
+  assert.equal(relationship.key, 'articles');
+  assert.equal(relationship.kind, 'hasMany');
 });
--- tests/unit/utils/date-helpers-test.js
+++ tests/unit/utils/date-helpers-test.js
@@ -1,10 +1,13 @@
 import dateHelpers from '../../../utils/date-helpers';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | date helpers');
+module('Utils: formatDate');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  var result = dateHelpers();
-  assert.ok(result);
+
+test('formats a date object', function(assert) {
+  var date = new Date('11-03-2014');
+  var result = dateHelpers.formatDate(date, 'ddd MMM DD YYYY');
+
+  assert.equal(result, 'Mon Nov 03 2014', 'returns a readable string');
+
 });
